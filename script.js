9979618f9cc306e7b5af690e0d63299c31e9c8ae
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-05-11 16:50:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(num){
+       let bin = 0;
+    let rem, i = 1, step = 1;
+    while (x != 0) {
+        rem = x % 2;
+        x = parseInt(x / 2);
+        bin = bin + rem * i;
+        i = i * 10;
+    }
+    console.log(`Binary: ${bin}`);
 }
-
-module.exports = threeSum;
+    
+  
+ return parseInt(binaryStr.split("").reverse().join(""))
+ 
+module.exports = decimalToBinary;
